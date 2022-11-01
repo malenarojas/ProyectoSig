a6b065fc928811f1ed329bdcb1d3b221f0bfde1c
Malena Rojas Camargo <MalenaRojasCamargo@hotmail.com>
2022-11-01 14:26:24 -0400

    empezando desde 0

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/screens/screens.dart';
 
-
-
-
-void main() => runApp(const MyApp());
+void main() => runApp( const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
+
 
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      //home: const CardScreen(),
-      initialRoute: 'home',
-      routes: {
-        'home':( BuildContext context) => const HomeScreen(),
-        'Listview1':( BuildContext context) => const listview1Screen(),
-        'Listview2':( BuildContext context) => const listview2Screen(),
-        'alert':( BuildContext context) => const  AlertScreen(),
-        'card':( BuildContext context) => const CardScreen(),
-
-      },
-      onGenerateRoute: (settings) => {
-        print(settings);
-      },
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
--- lib/screens/screens.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_application_2/screens/home_screen.dart';
-
-export 'package:flutter_application_2/screens/alert_screen.dart';
-export 'package:flutter_application_2/screens/card_screen.dart';
-export 'package:flutter_application_2/screens/listview1_screen.dart';
-export 'package:flutter_application_2/screens/listview2_screen.dart';
-export 'package:flutter_application_2/screens/home_screen.dart'; 
